0a10c1c8f67d0bca3bcfc400cfbdc049f59b221f
Thijs Boerma <thijs.boerma@sogeti.com>
2019-01-07 11:16:09 +0100

    Feature: added Clock service and altered the timer component to reflect the actual time.

--- src/app/shared/components/clock/clock.component.ts
+++ src/app/shared/components/clock/clock.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClockService } from '../../services/clock.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private clockSubscription$: Subscription;
+  time: Date;
+
+  constructor(private clockService: ClockService) { }
 
   ngOnInit() {
+    this.clockSubscription$ = this.clockService.getClock().subscribe(
+      time => this.time = time
+    );
+  }
+
+  ngOnDestroy() {
+    this.clockSubscription$.unsubscribe();
   }
 
 }
