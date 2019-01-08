f9a4919041a926e30f778411af74d34881ffddac
Thijs Boerma <thijs.boerma@sogeti.com>
2019-01-08 16:25:58 +0100

    Removed unused imports from app.module, added an error page for 404 which employs the shared grid items.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridModule } from './home-grid/grid.module';
-import { Routes, RouterModule } from '@angular/router';
-import { GridComponent } from './home-grid/grid.component';
 import { SharedModule } from './shared/shared.module';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { AdminGridModule } from './admin-grid/admin-grid.module';
-import { AdminGridComponent } from './admin-grid/admin-grid.component';
 
 @NgModule({
   declarations: [
