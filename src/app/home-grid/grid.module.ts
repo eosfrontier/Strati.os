d8f765ae424ccd9dc9a97b9c7658da74b682d4ee
Thijs Boerma <thijs.boerma@sogeti.com>
2020-04-06 13:30:57 +0200

    fix transitions, remove deprecated footer-left

--- src/app/home-grid/grid.module.ts
+++ src/app/home-grid/grid.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GridComponent } from './grid.component';
 import { SideComponent } from './side/side.component';
-import { FooterLeftComponent } from './footer-left/footer-left.component';
 
 import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './footer/footer.component';
@@ -12,7 +11,7 @@ import { OrderModule } from 'ngx-order-pipe';
 
 @NgModule({
   declarations: [
-    GridComponent, SideComponent, FooterLeftComponent, FooterComponent, MissionsComponent
+    GridComponent, SideComponent, FooterComponent, MissionsComponent
   ],
   imports: [
     CommonModule, SharedModule, FontAwesomeModule, OrderModule
