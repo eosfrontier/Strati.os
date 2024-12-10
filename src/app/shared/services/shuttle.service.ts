ff5e67d630bbbe098434f741d671b2aebac3365c
WafflesMcDuff <goldjd@gmail.com>
2024-12-10 22:30:53 +0100

    Cast headers as objejct

--- src/app/shared/services/shuttle.service.ts
+++ src/app/shared/services/shuttle.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Shuttle } from '../models/shuttle';
@@ -38,14 +38,15 @@ export class ShuttleService {
     }
 
     private getShuttlesFromAPI(): any {
-        const headerDict = {
-            'getAll': 'true'
-        }
+        const httpOptions : Object = {
+            headers: new HttpHeaders({
+              'getAll': 'true'
+            }),
+            responseType: 'text'
+          };
+          
 
-        const requestOptions = {
-            headers: new Headers(headerDict),
-        };
-        return this.http.get(environment.SHUTTLE.API_URL, requestOptions).pipe(map(res => {
+        return this.http.get(environment.SHUTTLE.API_URL, httpOptions).pipe(map(res => {
             return res
         }));
     }
